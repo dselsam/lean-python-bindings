323df59df6e6e61878b8c95e74f79bcdcde25eb4
Philipp Moritz <pcmoritz@gmail.com>
2017-08-25 09:54:27 -0700

    copy the C extension to the right place

--- setup.py
+++ setup.py
@@ -2,16 +2,30 @@ from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
+import glob
+import os
+import shutil
 import subprocess
 
 from setuptools import setup, find_packages, Distribution
 import setuptools.command.build_ext as _build_ext
 
 class build_ext(_build_ext.build_ext):
+
     def run(self):
+        # We build the C++ extension in a different directory to avoid
+        # having build artifacts in the package directory and make it
+        # easy to clean the build. This means we need to move the files
+        # here.
         subprocess.check_call(["./build.sh"])
+        for path in glob.glob("build/lean.cpython*"):
+            directory, filename = os.path.split(path)
+            destination = os.path.join("lean", filename)
+            print("Copying {} to {}".format(path, destination))
+            shutil.copy(path, destination)
 
 class BinaryDistribution(Distribution):
+
     def has_ext_modules(self):
         return True
 
